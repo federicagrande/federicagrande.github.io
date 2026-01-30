3d0dced89b7775299b0bbefb72e203922a613d90
federicagrande <federica.grande17@gmail.com>
2026-01-30 17:53:51 +0100

    Prima commit

---
+++ federica-portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import './portfolioFederica'
+import './portfolioFederica.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bgColor"> 
+      <PortfolioFederica />
+    </div>
+      
+     
+    
+          )
+}
+
+export default App
